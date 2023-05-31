763d9a4241f378eb920d1640e28dea09af0df985
ponpon <pontus00valdes@gmail.com>
2023-05-31 18:02:28 +0200

    .

--- router/index.ts
+++ router/index.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const router = express.Router()
 
 router.post('/webhook', async (req, res) => {
-  console.log('req.body', req.body)
+  console.log('req.body', req)
   // res.status(200).json({ message: 'success' })
   return { output: 'yayy' }
 })
