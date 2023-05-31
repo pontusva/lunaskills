4302679262bf1b583e5c27274e665068e5823f31
ponpon <pontus00valdes@gmail.com>
2023-05-31 17:28:52 +0200

    .

--- router/index.ts
+++ router/index.ts
@@ -4,7 +4,7 @@ const router = express.Router()
 
 router.post('/webhook', (req, res) => {
   console.log(req.body)
-  res.status(200).json({ message: 'hello' })
+  // res.status(200).json({ message: 'hello' })
 
   return { output: req.body }
 })
