37ceca8199f5cdd1d2ed945d3cb06982c8afe646
ponpon <pontus00valdes@gmail.com>
2023-05-31 17:21:46 +0200

    try to modify the initial response from carter

--- router/index.ts
+++ router/index.ts
@@ -5,7 +5,7 @@ const router = express.Router()
 router.post('/webhook', (req, res) => {
   console.log(req.body)
   res.status(200).json({ message: 'hello' })
-  return { output: 'hello' }
+  return { output: req.body }
 })
 
 export default router
