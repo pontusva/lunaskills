1e8f3473a20d7b7e52dae416fd507ecba451b2b0
ponpon <pontus00valdes@gmail.com>
2023-05-31 18:30:14 +0200

    webhook working

--- router/index.ts
+++ router/index.ts
@@ -1,11 +1,25 @@
 import express from 'express'
+import { CoordinateModel } from '../mongodb/models/coordinateModel'
 
 const router = express.Router()
 
 router.post('/webhook', async (req, res) => {
-  console.log('req.body', res)
-
-  res.send({ output: 'test', skillData: 'test' })
+  try {
+    const coordinates = await CoordinateModel.find({})
+    console.log(coordinates)
+    res.send({ output: coordinates })
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 export default router
+
+// try {
+//   const coordinates = await CoordinateModel.find({})
+//   console.log(coordinates)
+//   res.status(200).json({ coordinates })
+//   return { output: 'coordinates' }
+// } catch (error) {
+//   console.log(error)
+// }
